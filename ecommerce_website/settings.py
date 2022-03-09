8a187f2f988f7ede016178feceda3b934f341127
AbdulQadir92 <a_qadir_jamali@hotmail.com>
2022-03-10 02:13:59 +0500

    Completed Basic Structure of E-Commerce Website

--- ecommerce_website/settings.py
+++ ecommerce_website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-eypxx1#d^kp77v9=t45x)s2un)32opjnbhd2ioyqrwm!ci5hw1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['e-commerce-321.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['e-commerce-321.herokuapp.com']
 
 
 # Application definition
